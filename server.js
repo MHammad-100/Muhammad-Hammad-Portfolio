a63e75270247383fc13f07303e31627ebcc9cf19
MHammad-100 <hammadmansha100@gmail.com>
2021-12-19 22:16:48 +0500

    first commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express =  require('express');
 const server = express();
 const dotenv = require("dotenv");
 const bodyparser = require("body-parser");
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 const path = require("path");
 // mongodb+srv://Hammad:Hammad@cluster0.pbfho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 // mongodb://localhost:27017/hamzamessages
