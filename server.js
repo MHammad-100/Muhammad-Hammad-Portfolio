631d60f4fa389eeb1d6d7bbade6a78bd671ac641
MHammad-100 <84732821+MHammad-100@users.noreply.github.com>
2022-05-15 17:58:24 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ const dotenv = require("dotenv");
 const bodyparser = require("body-parser");
 const PORT = process.env.PORT || 5000;
 const path = require("path");
-// mongodb+srv://Hammad:Hammad@cluster0.pbfho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 // mongodb://localhost:27017/hamzamessages
-mongoose.connect("mongodb+srv://Hammad:Hammad@cluster0.pbfho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
+mongoose.connect("mongodb://localhost:27017/hamzamessages",{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(()=>{
